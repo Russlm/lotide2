e9fb5f45a12952d3cc8e2b4fe3441089a69f709d
Russell Mair <russell.e.mair@gmail.com>
2021-11-30 08:29:44 +0000

    updated Tail and Middle to work with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,31 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
 
-const tail= require("../tail");
+// const tail= require("../tail");
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case 2:
-const result2 = tail(["Experiment", "detonation", "Labs"]);
-assertEqual(result2.length, 2); // ensure we get back two elements
-assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
+// // Test Case 2:
+// const result2 = tail(["Experiment", "detonation", "Labs"]);
+// assertEqual(result2.length, 2); // ensure we get back two elements
+// assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3, 4, 5, 6] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5, 6]), [2, 3, 4, 5, 6]);
+  });
+  it("returns Lighthouse, Labs for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
+  // it("returns [] for [1]", () => {
+  //   assert.strictEqual(middle([1]), []); 
+  // });
+});
