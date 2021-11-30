5e612c1cb8b3771c59e807697d45f0af0cb4870a
Russell Mair <russell.e.mair@gmail.com>
2021-11-30 07:06:30 +0000

    updated head to include chai and mocha. strange bugs haha

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,18 @@
-const head = require("../head");
-const assertEqual = require("../assertEqual");
+// const head = require("../head");
+// const assertEqual = require("../assertEqual");
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), ["Hello", "Lighthouse", "Labs"]);
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 7 for [7, 6, 5]", () => {
+    assert.strictEqual(head([7, 6, 5]), 7);
+  });
+  it("returns hello for [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+});
+
